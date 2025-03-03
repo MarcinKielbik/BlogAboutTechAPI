22e5aa71ff4cbd6177ea81a29e816b563245915d
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-03-03 18:13:17 +0100

    Create Category Model

--- BlogAboutTechAPI/Models/BlogPost.cs
+++ BlogAboutTechAPI/Models/BlogPost.cs
@@ -2,5 +2,15 @@
 {
     public class BlogPost
     {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool IsVisible { get; set; }
+
     }
 }
