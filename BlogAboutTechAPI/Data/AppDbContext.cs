244190a6ac9bf06833ab0a1206ef19840606f53e
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-03-03 18:21:41 +0100

    Create AppDbContext for Data context

---
+++ BlogAboutTechAPI/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BlogAboutTechAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAboutTechAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+
+
+    }
+}
